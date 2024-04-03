3e8156dd4c29d36bfa9436142947075884a899fb
Serafadam <serafadam@gmail.com>
2024-04-03 06:44:59 +0000

    hunter

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,16 @@
+hunter_config(
+    Eigen
+    VERSION 3.4.0
+)
+
+hunter_config(
+    Sophus
+    URL "https://github.com/strasdat/Sophus/archive/936265f5815dee546a3583ceba58c234c2ecb271.zip"
+    SHA1 "7f54e06fb1680910c465e12ea85fc414ce7a9f9e" 
+)
+
+hunter_config(
+    cereal
+    URL "https://github.com/USCiLab/cereal/archive/64f50dbd5cecdaba785217e2b0aeea3a4f1cdfab.zip"
+    SHA1 "6587337e36045f1b5f68c902f5e04a76d8999e02"
+)
