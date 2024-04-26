55d2a60f8e63a03309c6c27f65932bc6c986115b
Serafadam <serafadam@gmail.com>
2024-04-26 15:25:33 +0000

    add versions to packages

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -5,12 +5,14 @@ hunter_config(
 
 hunter_config(
     Sophus
+    VERSION 1.1.0
     URL "https://github.com/strasdat/Sophus/archive/936265f5815dee546a3583ceba58c234c2ecb271.zip"
     SHA1 "7f54e06fb1680910c465e12ea85fc414ce7a9f9e" 
 )
 
 hunter_config(
     cereal
+    VERSION 1.3.0
     URL "https://github.com/USCiLab/cereal/archive/64f50dbd5cecdaba785217e2b0aeea3a4f1cdfab.zip"
     SHA1 "6587337e36045f1b5f68c902f5e04a76d8999e02"
 )
